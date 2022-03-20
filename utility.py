ebbfa648ef354786b20a31b0dd3bcfde4d2b6b75
Mohammad Rashid <mdrsd97@gmail.com>
2022-03-20 20:09:21 +0530

    Initial Commit

---
+++ utility.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def add(a,b):
+    return a+b
+
+
+# In[2]:
+
+
+def sub(a,b):
+    return a-b
+
+
+# In[3]:
+
+
+def mul(a,b):
+    return a*b
+
+
+# In[ ]:
+
+
+def div(a,b):
+    return a/b
+
